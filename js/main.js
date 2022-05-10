f5f591fbe5dc3cbaccefbdf0e0db631eea3abf58
abdisalomoff <Avazbek6757@gmail.com>
2022-05-10 22:06:37 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var menuBtn = document.querySelector('.site-header__burger-btn')
+var menuNavbar = document.querySelector('.navbar')
+
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle("navbar--open")
+    // menuBtn.classList.toggle("header-btn--open");
+    // menuBtn.classList.toggle("navbar-open__x");
+    
+})
